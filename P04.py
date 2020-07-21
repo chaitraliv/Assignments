3229ae25803acaf972e0907fda04e7066fa39d99
chaitraliv <chaitrali.vaidya@instazen.com>
2020-07-21 18:13:07 +0530

    list problem

--- P04.py
+++ P04.py
@@ -3,6 +3,6 @@
 
 mylist = ['a','b','c','d']
 count=0
-for element in mylist:	#for every element in the list, increment the counter
+for element in mylist:	        #for every element in the list, increment the counter
     count+=1
 print(count)
