7d6b677f6f75f70a36d654b948e64dfb6b4e34e5
chaitraliv <chaitrali.vaidya@instazen.com>
2020-07-27 15:30:48 +0530

    Arithmetic Problem

--- P33.py
+++ P33.py
@@ -1,26 +1,21 @@
-#SOLUTION FOR P33
-'''33 (*) Determine whether two positive integer numbers are coprime.
+'''P33 (*) Determine whether two positive integer numbers are coprime.
 Two numbers are coprime if their greatest common divisor equals 1.
 Example:
-* (coprime 35 64)
-T'''
+* (coprime 35 64)'''
+
 
 num1 = int(input('enter first number = '))
 num2 = int(input('enter second number = '))
 
-#FUNCTION TOCHECK IF GIVEN NUMBERS ARE CO-PRIME
-def check_coprime(n1,n2):
-    gcd = 0                                                 #COUNTER VARIABLE TO CHECK IF THEY ARE CO-PRIME
-    if num1 and num2 >=0:                                   #CONTINUE FOR POSITIVE INTEGERS
-        for i in range (2,(num1)):
-            if (num1 % i) ==0 and (num2 % i) == 0:          #IF THE NUMBERS HAVE A COMMON DIVISOR OTHER THAN 1,
-               gcd +=1                                      #INCREMENT COUNTER VARIABLE
-        if gcd == 0 :                                       #IF COUNTER IS NOT CHANGED, NUMBERS ARE CO-PRIME
-            print("Numbers are co-prime")
-        else :                                              #ELSE THE NUMBERS ARE NOT CO-PRIME
-            print("Numbers are not co-prime")
+#CREATE A FUNCTION TO CALCULATE GCD
+def find_gcd(n1,n2):
+    while n2!=0:            #BASE CASE IS - gcd(a,0) = a ; SO CHECK TILL N2 IS NOT ZERO
+        n1,n2= n2,n1%n2     #ACCORDING TO ALGORITH gcd(a,b)=gcd(b,r) where r=a%b
+        find_gcd(n1,n2)     #CONTINUE TO CALCULATE GCD FOR NEW N1 AND N2
+    return n1               #WHEN BASE CASE SATISFIES, RETURN N1
 
-    else:
-        print("Invalid input")
-
-result = check_coprime(num1,num2)                           #CALL THE FUNCTION
+res= find_gcd(num1,num2)    #CALL THE FUNCTION AND PASS TWO NUMBERS
+if res==1:                  #IF THE RESULTANT GCD IS ONE, NUMBERS ARE CO-PRIME
+    print(f'{num1} & {num2} are co-prime')
+else:                       #IF GCD IS OTHER THAN 1,NUMBERS ARE NOT CO-PRIME
+    print(f'{num1} & {num2} are not co-prime')
